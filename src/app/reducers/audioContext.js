00b70d768689cfba5af8e2d5068b2a83a5904375
skratchdot <git@skratchdot.com>
2015-12-13 13:18:44 -0500

    consistency with other unnamed functions

--- src/app/reducers/audioContext.js
+++ src/app/reducers/audioContext.js
@@ -1,6 +1,6 @@
 import * as types from '../constants/ActionTypes';
 
-export default function counter(state = new AudioContext(), action) {
+export default function (state = new AudioContext(), action) {
   switch (action.type) {
     case types.SET_AUDIO_CONTEXT:
       return action.value;
